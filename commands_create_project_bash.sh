bf302a898f9c00716195206039b3e2630d442c7d
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-08-06 16:28:30 +0200

    add bash script

---
+++ commands_create_project_bash.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+TEST = $1
+# $url_to_clone = $2
+# $project_type = $3
+
+# git clone $url_to_clone
+# mkdir $1
+# cd $repo_name
+# touch README.md
+
+# echo $repo_name > README.md
+
+# if [$project_type == "JS"]
+# then 
+#     touch index.html style.css index.js
+# elif [$project_type == "HTML/CSS"]
+# then
+#     touch index.html style.css
+# else
+#     touch main.py
+# fi
+
+# code .
