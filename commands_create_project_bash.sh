3968db0ed1d469e5022ac26d6875f7a6d64be649
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-08-07 10:04:52 +0200

    write bash script

--- commands_create_project_bash.sh
+++ commands_create_project_bash.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+repo_name=$1
+url_to_clone=$2
+project_type=$3
 
-TEST = $1
-# $url_to_clone = $2
-# $project_type = $3
 
 # git clone $url_to_clone
-# mkdir $1
-# cd $repo_name
-# touch README.md
+mkdir $repo_name
+cd $repo_name
+touch README.md
 
-# echo $repo_name > README.md
+echo "#$repo_name" > README.md
 
-# if [$project_type == "JS"]
-# then 
+if [ "$project_type" = "JS" ]; then
+    touch index.html style.css index.js
+elif [ "$project_type" = "HTML/CSS" ]; then
+    touch index.html style.css
+else
+    touch main.py
+fi
+
+# if ["$project_type" = "JS"];
+# then
 #     touch index.html style.css index.js
-# elif [$project_type == "HTML/CSS"]
+# elif [$project_type==HTML/CSS]
 # then
 #     touch index.html style.css
 # else
